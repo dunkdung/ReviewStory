49d219d1df97831175ce9d52d2fc3d3efc6c0911
wnstn0154@naver.com <wnstn0154@naver.com>
2021-05-19 01:07:23 +0900

    일단, 20210519 오전01시, 검색기능 개발중 리사이클러뷰 오류.. 실행은 됩니다.

--- app/src/main/java/com/example/reviewstory/search/SearchFragment.kt
+++ app/src/main/java/com/example/reviewstory/search/SearchFragment.kt
@@ -1,11 +1,20 @@
 package com.example.reviewstory.search
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.reviewstory.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.android.synthetic.main.search_item.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,7 +27,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    var firestore : FirebaseFirestore? = null
+    var fbAuth : FirebaseAuth? = null
+
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -29,7 +41,6 @@ class SearchFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,16 +49,97 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFragmentResultListener("key") { key, bundle ->
+            val result = bundle.getString("bundleKey")
+            fbAuth = FirebaseAuth.getInstance()
+            firestore = FirebaseFirestore.getInstance()
+            recyclerview.adapter = RecyclerViewAdapter()
+            recyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+
+            var searchOption = "s_name"
+            spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    when (spinner.getItemAtPosition(position)) {
+                        "주소" -> {
+                            searchOption = "address"
+                        }
+                        "이름" -> {
+                            searchOption = "s_name"
+                        }
+                    }
+                }
+            }
+        searchBtn.setOnClickListener {
+            (recyclerview.adapter as RecyclerViewAdapter).search(searchWord.text.toString(), searchOption)
+        }
+
+
+
+        }
+    }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        // Person 클래스 ArrayList 생성성
+        var stamp: ArrayList<Stamp_data> = arrayListOf()
+
+        init {  // telephoneBook의 문서를 불러온 뒤 Person으로 변환해 ArrayList에 담음
+            firestore?.collection("stamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                Log.d("place", "데이터 전달"+ querySnapshot!!.documents)
+                // ArrayList 비워줌
+                stamp.clear()
+
+                for (snapshot in querySnapshot!!.documents) {
+                    Log.d("place", "데이터 전달"+ snapshot)
+                    var item = snapshot.toObject(Stamp_data::class.java)
+                    stamp.add(item!!)
+                }
+                notifyDataSetChanged()
+            }
+        }
+        // xml파일을 inflate하여 ViewHolder를 생성
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.search_item, parent, false)
+            return ViewHolder(view)
+        }
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        }
+
+        // onCreateViewHolder에서 만든 view와 실제 데이터를 연결
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var viewHolder = (holder as ViewHolder).itemView
+
+            viewHolder.address.text = stamp[position].address
+            viewHolder.s_name.text = stamp[position].s_name
+        }
+
+        // 리사이클러뷰의 아이템 총 개수 반환
+        override fun getItemCount(): Int {
+            return stamp.size
+        }
+        fun search(searchWord : String, option : String) {
+            firestore?.collection("stamp")?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                // ArrayList 비워줌
+                stamp.clear()
+
+                for (snapshot in querySnapshot!!.documents) {
+                    if (snapshot.getString(option)!!.contains(searchWord)) {
+                        var item = snapshot.toObject(Stamp_data::class.java)
+                        stamp.add(item!!)
+                    }
+                }
+                notifyDataSetChanged()
+            }
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SearchFragment().apply {
@@ -57,4 +149,5 @@ class SearchFragment : Fragment() {
                 }
             }
     }
+
 }
