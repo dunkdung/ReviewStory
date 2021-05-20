1267cc017f66c83f459d46d40f546a5b4e8dfd0a
lee2689 <sayun0518@naver.com>
2021-05-21 06:08:09 +0900

    5월21일
    달력 검색

--- app/src/main/java/com/example/reviewstory/mypage/MypageAdapter.kt
+++ app/src/main/java/com/example/reviewstory/mypage/MypageAdapter.kt
@@ -1,24 +1,25 @@
-package com.example.reviewstory.search
+package com.example.reviewstory.mypage
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavDirections
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.reviewstory.R
 import com.example.reviewstory.REVIEW
+import com.example.reviewstory.TIMELINE
+import com.example.reviewstory.search.SearchFragmentDirections
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.list_item_review.view.*
+import kotlinx.android.synthetic.main.list_item_timeline.view.*
 
-/* ResultFragment에서 검색 결과를 리사이클러뷰에 데이터를 보여주는 어댑터  */
-class SearchAdapter(val items: ArrayList<REVIEW>, val fbFirestore: FirebaseFirestore) : RecyclerView.Adapter<ItemViewHolder>() {
+class MypageAdapter(val items: ArrayList<REVIEW>, val fbFirestore: FirebaseFirestore) : RecyclerView.Adapter<ItemViewHolder>() {
 
     /* 뷰홀더를 생성하여 반환 */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         //list_item_fresh 뷰 inflate
         val rootView =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_review, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item_timeline, parent, false)
         return ItemViewHolder(rootView)
     }
 
@@ -33,19 +34,18 @@ class SearchAdapter(val items: ArrayList<REVIEW>, val fbFirestore: FirebaseFires
 
 //뷰홀더 클래스 선언
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bindItems(stamp: REVIEW?, fbFirestore: FirebaseFirestore) {
-        stamp?.let {
-            itemView.txt_gongpan_info.text = stamp.address
-            itemView.txt_unit.text =
-                "${stamp.s_name}"
+    fun bindItems(timeline: REVIEW, fbFirestore: FirebaseFirestore) {
+        timeline?.let {
+            itemView.txt_gongpan_info.text = timeline.s_date
+            itemView.txt_unit.text = timeline.s_name
             //itemView.txt_min_price.text = "메세지 수: "
-            itemView.txt_avg_price.text =
-                "방문날짜: " + "${stamp.s_date}"
+            //itemView.txt_avg_price.text =
+                //"방문날짜: " + "${stamp.s_date}"
             //itemView.txt_max_price.text = "발신처: " + "${stamp.user_num}"
-            itemView.setOnClickListener{
+            /*itemView.setOnClickListener{
                 val direction: NavDirections = SearchFragmentDirections.actionSearchFragmentToDetailFragment2(stamp.d_id.toString(),stamp.tl_num.toString())
-                findNavController(itemView).navigate(direction)
-            }
+                Navigation.findNavController(itemView).navigate(direction)
+            }*/
         }
     }
-}//end of ItemViewHolder
+}
